4d71ad8362324fe4c169559e3559f5b2f4d6a0d9
Rishaan Kapoor <152417193+MTMAG11@users.noreply.github.com>
2026-09-04 18:18:30 -0700

    Added local motor accessing because all motor data is now local

--- src/rocket_sim/motor.py
+++ src/rocket_sim/motor.py
@@ -1,7 +1,12 @@
 from rocketpy import SolidMotor, Function
+from pathlib import Path
+
+motor_directory = Path(__file__).resolve().parents[2] / "data" / "motors"
+
+motor_file = motor_directory / "Klima_A6.eng"
 
 # Thrust properties
 comments, description, data_points = SolidMotor.import_eng(
-    r"C:/Users/Rishaan/Downloads/Klima_A6.eng"
+    str(motor_file)
 )
 get_thrust = Function(data_points, interpolation="linear", extrapolation="constant")
